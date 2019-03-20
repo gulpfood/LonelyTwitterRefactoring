50d32e3d80a9b1d171b2cbb0d5903d4080ba8d6a
Chris Baker <31747040+gulpfood@users.noreply.github.com>
2019-03-20 17:53:27 -0600

    Update NormalLonelyTweet.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalLonelyTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalLonelyTweet.java
@@ -4,11 +4,9 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.Date;
 
-public class NormalLonelyTweet implements Serializable {
+public class NormalLonelyTweet extends Tweet implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	protected Date tweetDate;
-	protected String tweetBody;
 
 	public NormalLonelyTweet() {
 	}
@@ -18,44 +16,14 @@ public class NormalLonelyTweet implements Serializable {
 		this.tweetBody = text;
 	}
 
-	public Date getTweetDate() {
-		return tweetDate;
-	}
-
-	public void setTweetDate(Date tweetDate) {
-		this.tweetDate = tweetDate;
-	}
-
-	public String getTweetBody() {
-		return tweetBody;
-	}
-
-	public void setTweetBody(String tweetBody) {
-		this.tweetBody = tweetBody;
-	}
-
-	private void writeObject(java.io.ObjectOutputStream out) throws IOException {
-		out.writeObject(tweetDate);
-		out.writeObject(tweetBody);
-	}
-
-	private void readObject(java.io.ObjectInputStream in) throws IOException,
-			ClassNotFoundException {
-		tweetDate = (Date) in.readObject();
-		tweetBody = (String) in.readObject();
-	}
-
-	public boolean isValid() {
-		if (tweetBody.trim().length() == 0
-				|| tweetBody.trim().length() > 10) {
+	@Override
+	public boolean isValidBody() {
+		if (getTweetBody().trim().length() == 0
+				|| getTweetBody().trim().length() > 10) {
 			return false;
 		}
 
 		return true;
 	}
 
-	@Override
-	public String toString() {
-		return getTweetDate() + " | " + getTweetBody() ;
-	}
 }
